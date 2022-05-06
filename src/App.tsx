9819903db4d18eb48930ba3040dc7b489b3dcd5a
Roman Pavlenko <rpavlenko.mail@gmail.com>
2022-05-06 17:16:32 +0200

    add header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,23 @@
-import { useContext } from 'react';
 import { BaseButton } from './ui/atoms/Button/BaseButton';
 import { Container } from '@mui/material';
-import { useTheme } from '@mui/material/styles';
-import { ColorModeContext } from './themes/ColorModeContext';
-import IconButton from '@mui/material/IconButton';
-import Brightness4Icon from '@mui/icons-material/Brightness4';
-import Brightness7Icon from '@mui/icons-material/Brightness7';
-import './index.css';
-import FilterListOutlinedIcon from '@mui/icons-material/FilterListOutlined';
 import { FilterButton } from './ui/atoms/Button/FilterButton';
+import FilterListOutlinedIcon from '@mui/icons-material/FilterListOutlined';
+import Header from './ui/organisms/Header';
+import './index.css';
 
 export default function App() {
-  const theme = useTheme();
-  const colorMode = useContext(ColorModeContext);
-
   return (
-    <div className='App'>
-      <Container maxWidth='sm'>
-        {theme.palette.mode} mode
-        <IconButton
-          sx={{ ml: 1 }}
-          onClick={colorMode.toggleColorMode}
-          color='inherit'
-        >
-          {theme.palette.mode === 'dark' ? (
-            <Brightness7Icon />
-          ) : (
-            <Brightness4Icon />
-          )}
-        </IconButton>
-        <header className='App-header'>
+    <div className="App">
+      <Header />
+      <Container
+        maxWidth="sm"
+        // sx={{
+        //   mt: 10,
+        // }}
+      >
+        <header className="App-header">
           <p>Hello Vite + React + MaterialUI!</p>
-          <BaseButton
-            variant={'outlined'} color={'primary'} size={'large'}
-            >
+          <BaseButton variant={'outlined'} color={'primary'} size={'large'}>
             Filtry
           </BaseButton>
           <FilterButton />

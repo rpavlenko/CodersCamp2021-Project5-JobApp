c20cb641ea873b0a998e1ea3a1542b8594b3217b
Roman Pavlenko <rpavlenko.mail@gmail.com>
2022-05-08 22:07:19 +0200

    add offers items list

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,48 @@ import { FilterButton } from './ui/atoms/Button/FilterButton';
 import { FiltersHeader } from './ui/atoms/FiltersHeader/FiltersHeader';
 import Header from './ui/organisms/Header';
 import './index.css';
-import { ListItem } from './ui/molecules/ListItem';
+import { OffersList } from './ui/organisms/OffersList';
 
 export default function App() {
+  const offersData = [
+    {
+      id: 1,
+      jobType: 'Zdalna',
+      jobTitle: 'Junior Developer',
+      companyTitle: 'Devire',
+      buttonTitle: '10000-15000 zł',
+      languageTitle: 'javascript',
+      logo: './src/assets/companyLogo.jpg',
+    },
+    {
+      id: 2,
+      jobType: 'Warszawa',
+      jobTitle: 'Junior Backend Developer',
+      companyTitle: 'SoftServe',
+      buttonTitle: '5000-10000 zł',
+      languageTitle: 'java',
+      logo: './src/assets/companyLogo.jpg',
+    },
+    {
+      id: 3,
+      jobType: 'Łódź',
+      jobTitle: 'Junior Frontend Developer',
+      companyTitle: 'Netguru',
+      buttonTitle: '7000-10000 zł',
+      languageTitle: 'python',
+      logo: './src/assets/companyLogo.jpg',
+    },
+    {
+      id: 4,
+      jobType: 'Wrocław',
+      jobTitle: 'Junior Web Developer',
+      companyTitle: 'LiveChat',
+      buttonTitle: '7000-10000 zł',
+      languageTitle: 'node',
+      logo: './src/assets/companyLogo.jpg',
+    },
+  ];
+
   return (
     <div className="App">
       <Header />
@@ -19,15 +58,9 @@ export default function App() {
             Filtry
           </BaseButton>
           <FilterButton />
-          <FiltersHeader name={'Kategoria'}/>
+          <FiltersHeader name={'Kategoria'} />
         </header>
-        <ListItem
-          jobType={'Zdalna'}
-          jobTitle={'Junior Developer'}
-          companyTitle={'Devire'}
-          buttonTitle={'10000-15000 zł'}
-          languageTitle={'javascript'}
-        />
+        <OffersList offers={offersData} />
       </Container>
     </div>
   );

b8065f5751cb948ebe3e52cc461f528cc8534c2c
Roman Pavlenko <rpavlenko.mail@gmail.com>
2022-05-10 23:45:34 +0200

    add api folder

---
+++ src/api/api.ts
@@ -0,0 +1,41 @@
+import logo from '../assets/companyLogo.jpg';
+
+// endpoint get:  /api/v1/offers
+export const offersData = [
+  {
+    id: 1,
+    jobType: 'Zdalna',
+    jobTitle: 'Junior Developer',
+    companyTitle: 'Devire',
+    buttonTitle: '10000-15000 zł',
+    languageTitle: 'javascript',
+    logo: logo,
+  },
+  {
+    id: 2,
+    jobType: 'Warszawa',
+    jobTitle: 'Junior Backend Developer',
+    companyTitle: 'SoftServe',
+    buttonTitle: '5000-10000 zł',
+    languageTitle: 'java',
+    logo: 'https://static.nofluffjobs.com/companies/logos/original/softserve_20220328_140653_20220328_165558.webp',
+  },
+  {
+    id: 3,
+    jobType: 'Łódź',
+    jobTitle: 'Junior Frontend Developer',
+    companyTitle: 'Netguru',
+    buttonTitle: '7000-10000 zł',
+    languageTitle: 'python',
+    logo: 'https://nofluffjobs.com/upload/listing/Netguru_20161004_141842.png',
+  },
+  {
+    id: 4,
+    jobType: 'Wrocław',
+    jobTitle: 'Junior Web Developer',
+    companyTitle: 'LiveChat',
+    buttonTitle: '7000-10000 zł',
+    languageTitle: 'node',
+    logo: 'https://static.nofluffjobs.com/companies/logos/original/logo_rgb_orange_20220420_142843.png',
+  },
+];

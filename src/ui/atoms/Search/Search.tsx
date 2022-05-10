d2562c83bec40a76edeef9de85cb8c73fef95a7a
Barbara <bkorytkowska@gmail.com>
2022-05-10 21:54:29 +0200

    search

---
+++ src/ui/atoms/Search/Search.tsx
@@ -0,0 +1,31 @@
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+
+export default function CustomizedInputBase() {
+  return (
+    <Paper
+      component="form"
+      sx={{
+        p: '2px 4px',
+        display: 'flex',
+        alignItems: 'center',
+        width: 400,
+        '&:hover': {
+          backgroundColor: '#ddd',
+        },
+      }}
+    >
+      <IconButton sx={{ p: '10px' }} aria-label="menu"></IconButton>
+      <InputBase
+        sx={{ ml: 1, flex: 1 }}
+        placeholder="Wpisz,aby wyszukać"
+        inputProps={{ 'aria-label': 'Wpisz,aby wyszukać' }}
+      />
+      <IconButton type="submit" sx={{ p: '10px' }} aria-label="search">
+        <SearchIcon />
+      </IconButton>
+    </Paper>
+  );
+}

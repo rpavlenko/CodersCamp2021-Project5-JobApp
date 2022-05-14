cb252bcb50cd643faff5aa3bb434877a431413bb
Roman Pavlenko <rpavlenko.mail@gmail.com>
2022-05-15 00:03:03 +0200

    change position off apply button to sticky

--- src/ui/Pages/OfferDetailPage.tsx
+++ src/ui/Pages/OfferDetailPage.tsx
@@ -4,27 +4,27 @@ import { BottomApplyFofAnOffer } from '../molecules/BottomApplyFofAnOffer/Bottom
 import AppBar from '@mui/material/AppBar';
 import { Box } from '@mui/material';
 import { BackIconButton } from '../atoms/Button/BackIconButton';
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
 export const OfferDetailPage = () => {
- let navigate = useNavigate();
- //TODO Dynamic ID
+  let navigate = useNavigate();
+  //TODO Dynamic ID
   const handleClick = () => {
-    navigate("/detail/:id/apply");
-  }
+    navigate('/detail/:id/apply');
+  };
   return (
     <>
       <Container>
         <Box sx={{ paddingBottom: '82px' }}>
-          <BackIconButton/>
-          <DetailHeader/>
+          <BackIconButton />
+          <DetailHeader />
         </Box>
-        <AppBar position='fixed' sx={{ top: 'auto', bottom: 0 }}>
-          <Container>
-            <BottomApplyFofAnOffer onClick={handleClick} />
-          </Container>
-        </AppBar>
       </Container>
+      <AppBar position="sticky" sx={{ top: 'auto', bottom: 0 }}>
+        <Container>
+          <BottomApplyFofAnOffer onClick={handleClick} />
+        </Container>
+      </AppBar>
     </>
   );
 };

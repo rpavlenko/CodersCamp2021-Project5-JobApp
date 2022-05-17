ff6d1d28fea52209e54c6ef81680d8071a2bde21
Roman Pavlenko <rpavlenko.mail@gmail.com>
2022-05-17 22:02:36 +0200

    add simple reducer with redux toolkit to app

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-
-import App from './App';
-import './index.css';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
 import ColorModeContextProvider from './themes/ColorModeContext';
 import CssBaseline from '@mui/material/CssBaseline';
+import App from './App';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ColorModeContextProvider>
       <BrowserRouter>
         <CssBaseline />
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </BrowserRouter>
     </ColorModeContextProvider>
   </React.StrictMode>,

6bcf40cdc7c09079206e139379952c388cc397ed
magdalenaz-c <zaniewska.cmagdalena@gmail.com>
2022-05-24 14:43:34 +0200

    Add functionality to clear MessageInput in FormPage

--- src/ui/atoms/InputForm/MessageInput.tsx
+++ src/ui/atoms/InputForm/MessageInput.tsx
@@ -1,14 +1,19 @@
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import { TextField } from '@mui/material';
+import { useRef } from 'react';
 
 export const MessageInput = () => {
+  const textInput = useRef<HTMLInputElement>(null);
+
   return (
     <Box sx={{ position: 'relative' }}>
       <TextField
-        label="Wiadomość do rekrutera"
-        color="primary"
-        variant="outlined"
+        label='Wiadomość do rekrutera'
+        color='primary'
+        variant='outlined'
+        type='text'
+        inputRef={textInput}
         required
         multiline
         minRows={5}
@@ -19,8 +24,13 @@ export const MessageInput = () => {
         }}
       />
       <Button
+        onClick={() => {
+          if( textInput.current !== null) {
+            textInput.current.value = '';
+          }
+        }}
         size={'small'}
-        color="secondary"
+        color='secondary'
         sx={{
           position: 'absolute',
           right: '20px',
